8fcfaa06d3f873b38dfb71765cc451bc939c4ec6
rodrigobaggi0 <rodrigo.npii@gmail.com>
2025-05-06 21:30:53 -0300

    ATT

---
+++ index.js
@@ -0,0 +1,23 @@
+let div = document.createElement('div')
+div_logo_profission.id ='logo-profission'
+
+let div_logo = document.createElement('div')
+div_logo.id = 'logo'
+
+let h3 = document.createElement('h3')
+h3.textContent  = 'Técnico em Informática para Internet'
+
+
+
+const btns_text = ["HOME", "PROJETOS", "FORMAÇÃO"]
+// btns_text[i]
+
+for(let i = 0; i < 3 ; i++){
+    let div = document.createElement('div')
+    div.button_header = 'button-hearder'
+    div.className = 'button-container'
+    div.className = 'spin spin-blur'
+
+
+
+}
