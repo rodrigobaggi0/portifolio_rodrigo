a9f5c70d9b6a77512badf4b4863ca698b66529d4
rodrigobaggi0 <rodrigo.npii@gmail.com>
2025-05-06 21:43:12 -0300

    ATT

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
+{
 let div = document.createElement('div')
-div_logo_profission.id ='logo-profission'
+div_logo_profission.id = 'logo-profission'
 
 let div_logo = document.createElement('div')
 div_logo.id = 'logo'
 
 let h3 = document.createElement('h3')
-h3.textContent  = 'Técnico em Informática para Internet'
+h3.textContent = 'Técnico em Informática para Internet'
 
 
 
 const btns_text = ["HOME", "PROJETOS", "FORMAÇÃO"]
 // btns_text[i]
 
-for(let i = 0; i < 3 ; i++){
+for (let i = 0; i < 3; i++) {
     let div = document.createElement('div')
     div.button_header = 'button-hearder'
     div.className = 'button-container'
@@ -20,4 +21,5 @@ for(let i = 0; i < 3 ; i++){
 
 
 
+}
 }
